749f696b3d42e19a4dca56462a09ddebbe845309
Vivekachooz <vivekachooz@gmail.com>
2023-02-12 23:00:46 +0530

    extra: Initial commit
    Change-Id: I9f8bd22758be792b78f5529c4ed9d386c5ddfb0a

---
+++ BoardConfigExtra.mk
@@ -0,0 +1,13 @@
+# Product
+ifndef TARGET_COPY_OUT_PRODUCT
+    TARGET_COPY_OUT_PRODUCT := system/product
+endif
+
+# System_ext
+ifndef TARGET_COPY_OUT_SYSTEM_EXT
+    TARGET_COPY_OUT_SYSTEM_EXT := system/system_ext
+endif
+
+## SELinux
+BOARD_SEPOLICY_DIRS += \
+    hardware/google/pixel-sepolicy/flipendo
---
+++ product.mk
@@ -0,0 +1,11 @@
+## Pixel Apps
+include vendor/extra/apps/GoogleApps.mk
+
+## Google Fonts
+include vendor/extra/google-fonts/GoogleFonts.mk
+
+# GMS
+include vendor/gapps/arm64/arm64-vendor.mk
+
+## Overlays
+PRODUCT_PACKAGE_OVERLAYS += vendor/extra/overlay
