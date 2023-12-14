02154ed73e9432025002ccabea2fe0d45cdbbd39
Vivekachooz <vivekachooz@gmail.com>
2023-12-14 17:58:22 +0530

    extra: Drop everything for now
    Change-Id: I067f54ffeb289022ebd57c96423fa6ac5bf507b7

--- product.mk
+++ product.mk
@@ -1,21 +1,21 @@
 ## Pixel Apps
-include vendor/extra/apps/GoogleApps.mk
+#include vendor/extra/apps/GoogleApps.mk
 
 ## Google Fonts
-include vendor/extra/google-fonts/GoogleFonts.mk
+#include vendor/extra/google-fonts/GoogleFonts.mk
 
 # GMS
-ifeq ($(WITH_GMS),true)
-$(call inherit-product-if-exists, vendor/gapps/arm64/arm64-vendor.mk)
-endif
+#ifeq ($(WITH_GMS),true)
+#$(call inherit-product-if-exists, vendor/gapps/arm64/arm64-vendor.mk)
+#endif
 
 ## ih8sn
 $(call inherit-product-if-exists, ih8sn/product.mk)
 
 # Oplus cam
-ifeq ($(WITH_OPLUSCAM),true)
-$(call inherit-product-if-exists, vendor/oplus/camera/opluscamera.mk)
-endif
+#ifeq ($(WITH_OPLUSCAM),true)
+#$(call inherit-product-if-exists, vendor/oplus/camera/opluscamera.mk)
+#endif
 
 ## Overlays
-PRODUCT_PACKAGE_OVERLAYS += vendor/extra/overlay
+#PRODUCT_PACKAGE_OVERLAYS += vendor/extra/overlay
